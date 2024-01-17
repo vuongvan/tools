e568730866a93f3cf47c52d3d8e6a104803b27c6
Fei Yu <maivanvuong@gmail.com>
2024-01-17 22:13:34 +0700

    Update ytdlp_exc.bat

--- ytdlp_exc.bat
+++ ytdlp_exc.bat
@@ -1,44 +1,49 @@
-@echo off
+#!/bin/bash
 
-:START
-ECHO.
-ECHO 1. Tai mp3
-ECHO 2. Tai video
-ECHO 3. Tai video kem sub (mkv)
-ECHO.
-set choice=
-set /p choice=Chon thao thac muon thuc hien: 
-if not '%choice%'=='' set choice=%choice:~0,1%
-if '%choice%'=='1' goto music
-if '%choice%'=='2' goto video
-if '%choice%'=='3' goto videosub
-ECHO "%choice%" Vui long chon lai thao tac muon thuc hien.
-ECHO.
-goto start
+#yt-dlp
+YTDLPBIN=yt-dlp.exe
+YTDLPBINURL=https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
+DIRCONF=dir.conf
+#COLORS
+RED='\033[0;31m'
+Green='\033[0;32m'
 
-:music
-cls
-ECHO Tai mp3
-ECHO.
-set /p YouTubeURL=Nhap Link Youtube: 
-yt-dlp --ffmpeg-location %ffmpeg_location% --extract-audio --audio-format mp3 %YouTubeURL%
-echo:
-echo:
-cls
-GOTO music
+ffmpeg_location=$(echo | awk 'NR==1' $DIRCONF )
 
-:video
+while (true)
+do
+	echo -e "\n${Green}Chọn thao tác cần thực hiện: ************"
+	echo "  1) Tải mp3"
+	echo "  2) Tải video kèm sub (mkv)"
+	echo "  3) Tải mp3 kèm tách chapter"
+	echo "  4) Thoát chương trình"
+	echo "  0) Tải lại file $YTDLPBIN"
+	read n
 
-:videosub
-cls
-ECHO Tai video kem sub
-ECHO.
-set /p YouTubeURL=Nhap Link Youtube: 
-yt-dlp --ffmpeg-location %ffmpeg_location% -N 6 --no-mtime --sponsorblock-mark intro,outro --embed-chapters --convert-subs srt --embed-subs --sub-langs vi,en -f 247+bestaudio/best/247 --merge-output-format mkv --extractor-args "youtube:lang=en" %YouTubeURL%
-echo:
-echo:
-cls
-GOTO videosub
+	case $n in
+	  1)
+		echo -e "${Green}Nhập link youtube cần tải mp3: " 
+		read -r YouTubeURL 
+		./yt-dlp.exe --ffmpeg-location $ffmpeg_location --extract-audio --audio-format mp3 -P $PWD/Download// $YouTubeURL
+		;;
+	  2)
+		echo -e "${Green}Nhập link youtube cần tải video: " 
+		read -r YouTubeURL 
+		./yt-dlp.exe --ffmpeg-location $ffmpeg_location --no-mtime --sponsorblock-mark intro,outro --embed-chapters --convert-subs srt --embed-subs --sub-langs vi,en -f 247+bestaudio/best/247 --merge-output-format mkv --extractor-args "youtube:lang=en" -P $PWD/Download// $YouTubeURL
+		;;
+	  3)
+		echo -e "${Green}Nhập link youtube cần tải mp3: " 
+		read -r YouTubeURL 
+		./yt-dlp.exe --ffmpeg-location $ffmpeg_location --extract-audio --audio-format mp3 -P $PWD/Download// --split-chapters -o "chapter:%(section_title)s.%(ext)s" $YouTubeURL
+		;;
+	  4)
+		exit ;;
+	 
+	  0)
+		echo -e "${Green}Đang tải xuống file $YTDLPBIN"
+		wget -q --show-progress $YTDLPBINURL -O  $YTDLPBIN;;
 
-:end
-pause
+	  *) 
+		echo "invalid option";;
+	esac
+done
